c7706f33c6bcda039f6c704a336e38920f5c54ed
tokisakininovn <trieuduytancbg@gmail.com>
2024-01-22 23:53:04 +0700

    Nino update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -27,3 +27,18 @@ document.addEventListener("DOMContentLoaded", () => {
             });
     });
 });
+
+
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+});
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+        e.preventDefault();
+    }
+});
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.keyCode === 85) {
+        e.preventDefault();
+    }
+});
