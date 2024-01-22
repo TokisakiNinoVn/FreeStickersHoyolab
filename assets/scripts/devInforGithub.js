fe82fa527ff153efa4e15aa31be3872283b1e692
tokisakininovn <trieuduytancbg@gmail.com>
2024-01-22 22:11:39 +0700

    nino update

--- assets/scripts/devInforGithub.js
+++ assets/scripts/devInforGithub.js
@@ -1,37 +1,3 @@
-// // Sử dụng GitHub API để lấy avatar
-// const usernameNino = "tokisakininovn";
-// const apiUrl = `https://api.github.com/users/${usernameNino}`;
-// const usernameMizu = "MiyagawaMizu";
-// const apiUrl_ = `https://api.github.com/users/${usernameMizu}`;
-
-// fetch(apiUrl)
-//     .then(response => response.json())
-//     .then(data => {
-//         const avatarUrl = data.avatar_url;
-//         const avatarElement = document.querySelector(".avt_github_nino");
-//         avatarElement.src = avatarUrl;
-//     })
-//     .catch(error => {
-//         console.error('>> Error: ', error);
-//     });
-// setInterval(fetch, 30000);
-
-// fetch(apiUrl_)
-//     .then(response => response.json())
-//     .then(data => {
-//         const avatarUrl = data.avatar_url;
-//         const avatarElement = document.querySelector(".avt_github_mizu");
-//         avatarElement.src = avatarUrl;
-//     })
-//     .catch(error => {
-//         console.error('>> Error: ', error);
-//     });
-// setInterval(fetch, 30000);
-
-
-
-
-
 
 function fetchAndSetAvatar(username, avatarElementClass) {
     const apiUrl = `https://api.github.com/users/${username}`;
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -128,11 +128,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const openFill = document.querySelector(".filter");
     const closeFill = document.querySelector(".close_fill");
     const fillbox = document.querySelector(".box_fill");
-
     openFill.addEventListener("click", () => {
         fillbox.classList.add("active");
     });
     closeFill.addEventListener("click", () => {
         fillbox.classList.remove("active");
     });
+
+
+    const inputSearch = document.querySelector(".input_search");
+    const clearInputBtn = document.querySelector(".clear_input");
+    const pasteBtn = document.querySelector(".paste");
+    clearInputBtn.addEventListener("click", function() {
+        inputSearch.value = "";
+    });
+    pasteBtn.addEventListener("click", function () { 
+        navigator.clipboard.readText().then(function(clipboardData) {
+                inputSearch.value = clipboardData;
+            }).catch(function(err) {
+                console.error('Unable to read clipboard data', err);
+            });
+    });
+
 });
