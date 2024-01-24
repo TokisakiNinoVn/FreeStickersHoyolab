b8d9d62df80d375b785534e02fc8c91377594666
tokisakininovn <trieuduytancbg@gmail.com>
2024-01-25 01:46:32 +0700

    nino

--- assets/scripts/data.js
+++ assets/scripts/data.js
@@ -1,10 +1,10 @@
-//
+//Form inforchar + search infor name: ['land', 'Archon?true:false', 'Vision','charname', 'nickname'],
 export const linkImgGenshinImpact = [
     {
         land: 'Mondstadt',
         male: [
             {
-                name: [''],
+                name: ['Mondstadt', 'Archon', 'Ameno', 'Venti'],
                 images: [
                     '',
                 ],
@@ -20,10 +20,10 @@ export const linkImgGenshinImpact = [
         ],
     },
     {
-        land: 'Liyue',
+        land: 'Liyue', 
         male: [
             {
-                name: [''],
+                name: ['Liyue', 'Geo', ],
                 images: [
                     '',
                 ],
@@ -31,7 +31,7 @@ export const linkImgGenshinImpact = [
         ],
         female: [
             {
-                name: [''],
+                name: ['Liyue', 'Electron', ],
                 images: [
                         '',
                 ],
@@ -39,10 +39,10 @@ export const linkImgGenshinImpact = [
         ],
     },
     {
-        land: 'Inazuma',
+        land: 'Inazuma', 
         male: [
             {
-                name: ['Kamisato', 'Kamisato Ayato', 'Ayato'],
+                name: ['Inazuma', 'Kamisato', 'Hydro', 'Kamisato Ayato', 'Ayato'],
                 images: [
                     'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/e2c20d37a90e5d3216bf237fc05516fd_787843168069341004.png',
                     '',
@@ -52,7 +52,7 @@ export const linkImgGenshinImpact = [
         ],
         female: [
             {
-                name: ['Ei', 'Raiden Shogun'],
+                name: ['Inazuma', 'Archon', 'Electron', 'Ei', 'Raiden Shogun'],
                 images: [
                     'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/4bb1fdde040fa06122b047dfe518ea7d_7012957660689990894.png',
                     'https://upload-bbs.mihoyo.com/upload/2022/02/23/c0739c8c34bae5b3ee8749ef77b9384e_5736952483423015425.png',
@@ -61,7 +61,7 @@ export const linkImgGenshinImpact = [
                 ],
             },
             {
-                name: ['Kamisato', 'Kamisato Ayaka', 'Ayaka'],
+                name: ['Inazuma', 'Kamisato', 'Cryo', 'Kamisato Ayaka', 'Ayaka'],
                 images: [
                     'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/82444ce1a66aa7ed8f09e38a960741c4_438748924680795038.png',
                     'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/bd754deeab9869e9ad3c9bb98f99e95c_5141700034784822446.png',
@@ -70,7 +70,7 @@ export const linkImgGenshinImpact = [
                 ],
             },
             {
-                name: ['Yoimiya'],
+                name: ['Inazuma', 'Pyro', 'Yoimiya'],
                 images: [
                     'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/376c4f4fd78003e32c84fb981af9e1d0_2744860432938038745.png',
                     'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/2ceacac07fb4a4f9d01d2d6a4fbbc2c8_4402617639030240345.png',
@@ -84,7 +84,7 @@ export const linkImgGenshinImpact = [
         land: 'Sumeru',
         male: [
             {
-                name: [''],
+                name: ['Sumeru', ''],
                 images: [
                     '',
                 ],
@@ -92,7 +92,7 @@ export const linkImgGenshinImpact = [
         ],
         female: [
             {
-                name: [''],
+                name: ['Sumeru', ''],
                 images: [
                         '',
                 ],
@@ -100,16 +100,26 @@ export const linkImgGenshinImpact = [
         ],
     },
     {
-        land: 'Fontaine',
+        land: 'Fontaine', 
         male: [
             {
-                name: ['Lynet'],
+                name: ['Fontaine', 'Pyro', 'Lynet'],
                 images: [
                     'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/0b9d14c640ec5fd5f3721f82efed088e_2439392095879388438.png',
                 ],
             },
             {
-                name: ['Freminet'],
+                name: ['Fontaine', 'Cryo', 'Băng', 'Freminet'],
+                images: [
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/ed7e49250ae0a57a27a8d829721c439f_2622842278920165888.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/ed7e49250ae0a57a27a8d829721c439f_2622842278920165888.png',
+                    '',
+                    '',
+                    '',
+                ],
+            },
+            {
+                name: ['Fontaine', 'Cryo', 'Freminet'],
                 images: [
                     'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/ed7e49250ae0a57a27a8d829721c439f_2622842278920165888.png',
                 ],
@@ -117,7 +127,7 @@ export const linkImgGenshinImpact = [
         ],
         female: [
             {
-                name: ['Chevreuse'],
+                name: ['Fontaine', 'Pyro', 'Chevreuse'],
                 images: [
                     'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/dfaa2e886216ec652d30b4639aaee997_7292341287344452809.png',
                     'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/b0bfc381a1c2595a4291faf99993a70f_998579751033297035.png',
@@ -126,17 +136,31 @@ export const linkImgGenshinImpact = [
                 ],
             },
             {
-                name: ['Furina'],
+                name: ['Fontaine', 'Archon', 'Hydro', 'Furina'],
                 images: [
-                        'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/a3dcdf76051dcad9d499fdf1e29ed607_7876722293804135744.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/a3dcdf76051dcad9d499fdf1e29ed607_7876722293804135744.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/e78a2d510c1e0f09dfbd2dfcd7b73df3_4138096101246945813.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/e1f5b1026d5a0ef8bf25af71e8371e14_1856346621648695686.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/66fb73e569b6b91bd398d253749583db_8867564860876177156.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/90ca98c34ad2c0c6c726faf028585d7f_7433266281512583972.png',
                 ],
             },
             {
-                name: ['Navia'],
+                name: ['Fontaine', 'Geo', 'Navia'],
                 images: [
-                        'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/039602f09ed96d24a84fb63cf1b87def_2336672188464040726.png',
-                        'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/6f28a208d7de0d6c5d1eab3ce7adbe46_6618849726039595790.png',
-                        'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/39119a8c276f089133e5d100cecda51b_7830854357821020713.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/039602f09ed96d24a84fb63cf1b87def_2336672188464040726.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/6f28a208d7de0d6c5d1eab3ce7adbe46_6618849726039595790.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/39119a8c276f089133e5d100cecda51b_7830854357821020713.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/e49db4cb8928e3a895567ea56d52963a_8429510825415113403.png',
+                ],
+            },
+            {
+                name: ['Fontaine', 'Charlotte'],
+                images: [
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/666405a46dc6028175061164a87e48c6_8041026194871798660.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/5142d3bbe0bae97ce9a2d4aa36d0e19a_5276004962372933640.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/11/17/6b7ffcc3f9cc92bbefe83d2d2599d38f_6144830342900167388.png',
+                    '',
                 ],
             },
         ],
