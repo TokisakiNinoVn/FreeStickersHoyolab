4ab88af9ca8f53010503459ee19f7ade5002b23e
tokisakininovn <trieuduytancbg@gmail.com>
2024-01-25 01:09:17 +0700

    nino

--- assets/scripts/data.js
+++ assets/scripts/data.js
@@ -43,8 +43,10 @@ export const linkImgGenshinImpact = [
         land: 'Inazuma',
         male: [
             {
-                name: [''],
+                name: ['Kamisato Ayato', 'Ayato'],
                 images: [
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/e2c20d37a90e5d3216bf237fc05516fd_787843168069341004.png',
+                    '',
                     '',
                 ],
             },
@@ -55,6 +57,26 @@ export const linkImgGenshinImpact = [
                 images: [
                     'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/4bb1fdde040fa06122b047dfe518ea7d_7012957660689990894.png',
                     'https://upload-bbs.mihoyo.com/upload/2022/02/23/c0739c8c34bae5b3ee8749ef77b9384e_5736952483423015425.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/b6d6262e07c683baa94d7ab94a0795d0_2557740392923490910.png',
+                    '',
+                ],
+            },
+            {
+                name: ['Kamisato Ayaka', 'Ayaka'],
+                images: [
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/82444ce1a66aa7ed8f09e38a960741c4_438748924680795038.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/bd754deeab9869e9ad3c9bb98f99e95c_5141700034784822446.png',
+                    '',
+                    '',
+                ],
+            },
+            {
+                name: ['Yoimiya'],
+                images: [
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/376c4f4fd78003e32c84fb981af9e1d0_2744860432938038745.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/2ceacac07fb4a4f9d01d2d6a4fbbc2c8_4402617639030240345.png',
+                    '',
+                    '',
                 ],
             },
         ],
@@ -98,7 +120,24 @@ export const linkImgGenshinImpact = [
             {
                 name: ['Chevreuse'],
                 images: [
-                        '',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/dfaa2e886216ec652d30b4639aaee997_7292341287344452809.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/b0bfc381a1c2595a4291faf99993a70f_998579751033297035.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/4374b5b9d950a7eef1723bf8470387b6_5528635137601829273.png',
+                    'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/676f6b9a38f8acc4c5f79fe5c4f3370f_6450399376259269410.png',
+                ],
+            },
+            {
+                name: ['Furina'],
+                images: [
+                        'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/a3dcdf76051dcad9d499fdf1e29ed607_7876722293804135744.png',
+                ],
+            },
+            {
+                name: ['Navia'],
+                images: [
+                        'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/039602f09ed96d24a84fb63cf1b87def_2336672188464040726.png',
+                        'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/6f28a208d7de0d6c5d1eab3ce7adbe46_6618849726039595790.png',
+                        'https://upload-os-bbs.hoyolab.com/upload/2023/12/28/39119a8c276f089133e5d100cecda51b_7830854357821020713.png',
                 ],
             },
         ],
